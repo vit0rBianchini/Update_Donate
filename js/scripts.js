0e56cb3c23161931497b6a1e5e7688db978dfe6c
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2021-09-22 10:04:42 -0300

    Começando o projeto

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const home = document.getElementById('icon-home');
+
+const toCadastro = () => {
+    window.location.href = '/pages/cadastro.html'
+}
+
+const toggleClassHome = ()=>{
+    const homeBg = document.getElementById('home-bg');
+    homeBg.classList.toggle('backgrand-ativo')
+    const cabNav = document.getElementById('cabNav')
+    cabNav.classList.toggle('cabecalho_navegacao-ativo')
+    const body = document.getElementById('body');
+    body.classList.toggle('body-ativo')
+}
